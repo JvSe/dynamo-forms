850ed5d95b18854e32255aa47c93c82de535b033
João Vitor Soares <joaovitorsoares12@gmail.com>
2026-04-28 02:13:49 -0300

    Refactor InputDivider component to accept custom styles and props, adjusting margin for improved layout consistency.

--- packages/react-native/src/components/ui/input-divider.tsx
+++ packages/react-native/src/components/ui/input-divider.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ViewProps } from "react-native";
 
-export const InputDivider: React.FC = () => {
+export const InputDivider: React.FC = ({ style, ...rest }: ViewProps) => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, style]} {...rest}>
       <View style={styles.line} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { width: "100%", marginVertical: 24 },
+  container: { width: "100%", marginVertical: 5 },
   line: { height: 1, backgroundColor: "#d1d5db" },
 });
 
